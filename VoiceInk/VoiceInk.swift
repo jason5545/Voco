894d65b0268e94cbfa1f018a614bc5fcdb69a121
jason chien <jason5545@gmail.com>
2026-06-09 08:28:26 +0800

    Fix CloudKit container: use AppIdentifiers.cloudKitContainer
    Upstream merge hardcoded iCloud.com.prakashjoshipax.VoiceInk; our fork
    uses iCloud.com.jasonchien.Voco. Mismatch caused dictionary store to
    fail opening silently, falling back to empty in-memory db.

--- VoiceInk/VoiceInk.swift
+++ VoiceInk/VoiceInk.swift
@@ -211,7 +211,7 @@ struct VoiceInkApp: App {
             #if LOCAL_BUILD
             let dictionaryCloudKit: ModelConfiguration.CloudKitDatabase = .none
             #else
-            let dictionaryCloudKit: ModelConfiguration.CloudKitDatabase = .private("iCloud.com.prakashjoshipax.VoiceInk")
+            let dictionaryCloudKit: ModelConfiguration.CloudKitDatabase = .private(AppIdentifiers.cloudKitContainer)
             #endif
             let dictionaryConfig = ModelConfiguration(
                 "dictionary",
